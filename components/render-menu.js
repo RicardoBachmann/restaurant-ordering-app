e136bb9491dbd56e7c841295c6e8e22e8cf9d8e3
RicardoBachmann <ricardo.bachmann@icloud.com>
2023-10-18 12:37:46 +0200

    Add base styles for menu items

--- components/render-menu.js
+++ components/render-menu.js
@@ -5,9 +5,13 @@ export function renderMenu(menu) {
     menuEl.innerHTML += `
         <div class="menu-item">
             <div class="menu-item-icon">${item.emoji}</div>
-            <div class="menu-item-name">${item.name}</div>
-            <div class="menu-item-info">${item.ingredients.join(", ")}</div>
-            <div class="menu-item-price">${item.price}</div>
+              <div class="menu-item-info">
+                <div class="menu-item-name">${item.name}</div>
+                <div class="menu-item-ingredients">${item.ingredients.join(
+                  ", "
+                )}</div>
+                <div class="menu-item-price">$${item.price}</div>
+              </div>
         </div>`;
   });
 }
